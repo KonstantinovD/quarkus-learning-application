3bbf8015b39318ef7909768d7d32601060a95945
konstantinov_daniil407@mail.ru <konstantinov_daniil407@mail.ru>
2021-07-10 15:16:49 +0300

    [L] send requests to remote Rest service

---
+++ src/main/java/org/acme/beans/restclient/HttpBinServiceDAO.java
@@ -0,0 +1,51 @@
+package org.acme.beans.restclient;
+
+import org.acme.beans.jaxrsfilters.LoggingFilter;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.concurrent.CompletionStage;
+
+// We create a MicroProfile REST client. It starts and ends with
+//an interface:
+@RegisterRestClient(baseUri = "https://httpbin.org/")
+// This interface is a rest client stub for MicroProfile/CDI to pick up.
+// The baseURI configuration is the target REST resource’s base address.
+// With this annotation, I can later @Inject this interface wherever I
+// need to connect to baseUri.
+// Shout out to Kenneth Reitz, the proprietor of www.httpbin.org.
+// Httpbin.org is an HTTP testing web service for Requests
+// (Reitz’s library), a python HTTP library. It allows devs to test
+// their code against what’s essentially a mock web service. So, you can
+// copy-paste this code as is and it will have a service to talk to.
+@RegisterProvider(LoggingFilter.class)
+// configures the supplied class as a JAX-RS provider for this interface.
+public interface HttpBinServiceDAO {
+
+    @GET
+    // @GET defines this method for executing a GET request against the
+    // defined @Path. Remember, this will be appended to the baseURI, so
+    // that the final request URI in this example will read as
+    // https://httpbin.org/image/jpeg.
+    @Path("/image/jpeg")
+    @Produces("image/jpeg")
+    // The CompletionStage return type signals to the application
+    //runtime that this request is a reactive one. Used it here to wrap
+    //the binary data in the response from httpbin.org, for that specific
+    //endpoint. Flagging this as reactive makes this request asynchronous,
+    //event-driven, and CPU-efficient. The best of three worlds.
+    CompletionStage<byte[]> getImageAsync();
+
+    @PATCH
+    @Path("delay/{delay}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Response patchRandom(@PathParam("delay") int delay) throws Exception;
+
+    @POST
+    @Path("anything/{anything}")
+    @Produces(MediaType.TEXT_PLAIN)
+    Response postAnything(@PathParam("anything") String anything);
+}
---
+++ src/main/java/org/acme/controllers/RestClientController.java
@@ -0,0 +1,68 @@
+package org.acme.controllers;
+
+import org.acme.beans.restclient.HttpBinServiceDAO;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.annotations.cache.Cache;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@Path("/restclient")
+@Tag(name = "RestClientController",
+  description = "controller to send requests to another rest service")
+public class RestClientController {
+
+    // inject the REST client interface and mark it with the @RestClient
+    // annotation. Again, MicroProfile REST clients are available in any
+    // part of the Quarkus application; it doesn’t have to be inside of
+    // a JAX-RS resource class.
+    @Inject
+    @RestClient
+    HttpBinServiceDAO httpBinService;
+
+    @GET
+    @Path("/hello-image")
+    @Cache(maxAge = 30)
+    @Operation(summary = "Returns an image")
+    @Produces("image/jpg")
+    public Response helloImage()
+      throws InterruptedException, ExecutionException {
+        CompletionStage<byte[]> futureImage =
+          httpBinService.getImageAsync();
+        byte[] imageBytes = futureImage.toCompletableFuture().get();
+        // use the generic Response object to construct a HTTP response
+        // fit to carry an image. This is supported purely with the
+        // JAX-RS standard. Simply pass an instance of StreamingOutput
+        // to the entity method of Response.
+        return Response.ok().entity((StreamingOutput) outputStream -> {
+          outputStream.write(imageBytes);
+          outputStream.flush();
+        }).build();
+    }
+
+    @PATCH
+    @Path("/delay/{delay}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response patchRandom(@PathParam("delay") int delay)
+      throws Exception {
+        return httpBinService.patchRandom(delay);
+    }
+
+    @POST
+    @Path("/anything/{anything}")
+    public Response postAnything(@PathParam("anything") String anything) {
+        return httpBinService.postAnything(anything);
+    }
+
+
+
+}
